0d6b95b8c7534518b646d47d8d8000c59805bef3
sliabraaten <seth100@liabraaten.info>
2023-03-30 12:19:28 -0500

    added forward functionality

--- WalkerScript/WalkerScript/Program.cs
+++ WalkerScript/WalkerScript/Program.cs
@@ -22,62 +22,88 @@ namespace IngameScript
 {
     partial class Program : MyGridProgram
     {
-        // This file contains your actual script.
-        //
-        // You can either keep all your code here, or you can create separate
-        // code files to make your program easier to navigate while coding.
-        //
-        // In order to add a new utility class, right-click on your project, 
-        // select 'New' then 'Add Item...'. Now find the 'Space Engineers'
-        // category under 'Visual C# Items' on the left hand side, and select
-        // 'Utility Class' in the main area. Name it in the box below, and
-        // press OK. This utility class will be merged in with your code when
-        // deploying your final script.
-        //
-        // You can also simply create a new utility class manually, you don't
-        // have to use the template if you don't want to. Just do so the first
-        // time to see what a utility class looks like.
-        // 
-        // Go to:
-        // https://github.com/malware-dev/MDK-SE/wiki/Quick-Introduction-to-Space-Engineers-Ingame-Scripts
-        //
-        // to learn more about ingame scripts.
+        IMyExtendedPistonBase leftPistonX, leftPistonY, rightPistonX, rightPistonY;
+        IMyLandingGear rightMagPlate, leftMagPlate;
+        bool leftLegActive;
 
         public Program()
         {
-            // The constructor, called only once every session and
-            // always before any other method is called. Use it to
-            // initialize your script. 
-            //     
-            // The constructor is optional and can be removed if not
-            // needed.
-            // 
-            // It's recommended to set Runtime.UpdateFrequency 
-            // here, which will allow your script to run itself without a 
-            // timer block.
+            //AutoUpdate
+            Runtime.UpdateFrequency = UpdateFrequency.Update10;
+
+            leftPistonX = GridTerminalSystem.GetBlockWithName("Piston X Left") as IMyExtendedPistonBase;
+            leftPistonY = GridTerminalSystem.GetBlockWithName("Piston Y Left") as IMyExtendedPistonBase;
+            rightPistonX = GridTerminalSystem.GetBlockWithName("Piston X Right") as IMyExtendedPistonBase;
+            rightPistonY = GridTerminalSystem.GetBlockWithName("Piston Y Right") as IMyExtendedPistonBase;
+            rightMagPlate = GridTerminalSystem.GetBlockWithName("Mag Plate Right") as IMyLandingGear;
+            leftMagPlate = GridTerminalSystem.GetBlockWithName("Mag Plate Left") as IMyLandingGear;
+
+
+            if (rightMagPlate.IsLocked && leftMagPlate.IsLocked)
+                rightMagPlate.Unlock();
+
+            LegInit(leftPistonX, leftPistonY, leftMagPlate);
+            LegInit(rightPistonX, rightPistonY, rightMagPlate);
+
+            leftLegActive = true;
         }
 
         public void Save()
         {
-            // Called when the program needs to save its state. Use
-            // this method to save your state to the Storage field
-            // or some other means. 
-            // 
-            // This method is optional and can be removed if not
-            // needed.
         }
 
         public void Main(string argument, UpdateType updateSource)
         {
-            // The main entry point of the script, invoked every time
-            // one of the programmable block's Run actions are invoked,
-            // or the script updates itself. The updateSource argument
-            // describes where the update came from. Be aware that the
-            // updateSource is a  bitfield  and might contain more than 
-            // one update type.
-            // 
-            // The method itself is required, but the arguments above
-            // can be removed if not needed.
+
+
+            if (leftLegActive)
+            {
+                StepLeg(leftPistonX, leftPistonY, leftMagPlate, rightMagPlate);
+                ResetLeg(rightPistonX, rightPistonY);
+            }
+            else
+            {
+                StepLeg(rightPistonX, rightPistonY, rightMagPlate, leftMagPlate);
+                ResetLeg(leftPistonX, leftPistonY);
+            }
+            leftLegActive = !leftLegActive;
+
+
         }
+
+        //initializes the components of the leg to their retracted state
+        public void LegInit (IMyExtendedPistonBase xPiston, IMyExtendedPistonBase yPiston, IMyLandingGear magPlate)
+        {
+            if(!magPlate.IsLocked)
+                yPiston.Retract(); 
+            xPiston.Retract();
+        }
+
+        //steps the leg with the given pistons returns true if the step was properly executed 
+        public bool StepLeg(IMyExtendedPistonBase xPiston, IMyExtendedPistonBase yPiston, IMyLandingGear steppingMagPlate, IMyLandingGear retractingMagPlate)
+        {
+
+            yPiston.Extend();
+            steppingMagPlate.Lock();
+
+            //if plate is locked a successful step was made, otherwise the step was not successful 
+            if (steppingMagPlate.IsLocked)
+            {
+                retractingMagPlate.Unlock();
+                xPiston.Extend();
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public void ResetLeg(IMyExtendedPistonBase xPiston, IMyExtendedPistonBase yPiston)
+        {
+            yPiston.Retract();
+            xPiston.Retract();
+        }
+
+        //
+
     }
 }
